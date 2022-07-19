8812c2cb8c0500fb55e5c965241521658e9a4c96
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-07-19 16:15:07 +0200

    Initial commit

---
+++ Aludra.DesktopGL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Aludra.Core;
+
+namespace Aludra.DesktopGL
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using var game = new AludraGame();
+            game.Run();
+        }
+    }
+}
